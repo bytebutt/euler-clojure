8087637e852c0e03682865bba167a4c404b7e9f6
Miles Richardson <miles@bytebutt.com>
2016-04-13 16:13:18 -0400

    Rewrite primes/factorize and primes/sieve to be single-arity

--- src/project_euler/tools/numbers/primes.clj
+++ src/project_euler/tools/numbers/primes.clj
@@ -21,37 +21,39 @@
 
 (defn factorize
   "Returns a vector of the prime factors of `n`."
-  ([n]
-   (factorize n primes []))
-  ([n [x & xs :as candidates] factors]
-   (cond
-     ; No factors or fully factorized.
-     (<= n 1)
-     factors
-     ; Multiple instances of candidate prime factor.
-     (divisible-by (/ n x) x)
-     (recur (/ n x) candidates (conj factors x))
-     ; Single instance of candidate prime factor.
-     (divisible-by n x)
-     (recur (/ n x) xs (conj factors x))
-     ; Candidate prime is not a factor.
-     :else
-     (recur n xs factors))))
+  [n]
+  (loop [factors []
+         [x & xs :as candidates] primes
+         current n]
+    (cond
+      ; No factors or fully factorized.
+      (<= current 1)
+      factors
+      ; Multiple instances of candidate prime factor.
+      (divisible-by (/ current x) x)
+      (recur (conj factors x) candidates (/ current x))
+      ; Single instance of candidate prime factor.
+      (divisible-by current x)
+      (recur (conj factors x) xs (/ current x))
+      ; Candidate prime is not a factor.
+      :else
+      (recur factors xs current))))
 
 (defn sieve
   "Returns a vector of prime numbers between 2 and `n` inclusive."
-  ([n]
-   (if (< n 2)
-     []
-     (sieve [2] (range 3 (inc n) 2) (Math/sqrt n))))
-  ([primes [x & xs :as candidates] stop]
-   (cond
-     ; All candidates have been processed.
-     (empty? candidates)
-     primes
-     ; Hit the stopping point, so remaining candidates must be prime.
-     (> x stop)
-     (concat primes candidates)
-     ; Filter out multiples of current prime `x`.
-     :else
-     (recur (conj primes x) (filter #((complement divisible-by) % x) xs) stop))))
+  [n]
+  (if (< n 2)
+    []
+    (loop [primes [2]
+           [x & xs :as candidates] (range 3 (inc n) 2)
+           stop (Math/sqrt n)]
+      (cond
+        ; All candidates have been processed.
+        (empty? candidates)
+        primes
+        ; Hit the stopping point, so remaining candidates must be prime.
+        (> x stop)
+        (concat primes candidates)
+        ; Filter out multiples of current prime `x`.
+        :else
+        (recur (conj primes x) (filter #((complement divisible-by) % x) xs) stop)))))
